448545cf3ab2b8ab15bef440e9af57080ea0b71e
Luke Ma <lukemm23@gmail.com>
2019-09-30 14:28:38 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+// this line below directs DOM to render the app file and the render it in the index html file the div with the root ID
+ReactDOM.render(<App />, document.getElementById("root"));
